4619461c104463da1005a75889bd7e4d47d4c0da
abhinavshrivastava12 <abhinavshrivastava182@gmail.com>
2025-08-11 13:38:16 +0530

    jobboard

---
+++ backend/models/Application.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const applicationSchema = new mongoose.Schema(
+  {
+    jobId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Job",
+      required: true,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["Pending", "Accepted", "Rejected"],
+      default: "Pending",
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Application", applicationSchema);
